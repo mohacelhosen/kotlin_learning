bbd755d7f9655438e62c8459d63a0f804fb1ae18
mohacelhosen <mohacel.hosen22@gmail.com>
2024-01-07 13:02:51 +0600

    Reserve words

---
+++ src/main/kotlin/ReserveWords.kt
@@ -0,0 +1,7 @@
+
+fun main(){
+    val keywords = arrayOf("as", "as?", "break", "class", "continue", "do", "else", "false", "for", "fun", "if", "in", "!in", "interface", "is", "!is", "null", "object", "package", "return", "super", "this", "throw", "true", "try", "typealias", "val", "var", "when", "while")
+    for(keyword in keywords){
+        println("Kotlin keyword = $keyword")
+    }
+}
