9bdf222c7b8f064d062f3b9dcaef4809cb5e4fa9
mohacelhosen <mohacel.hosen22@gmail.com>
2024-01-07 13:22:30 +0600

    Print statement

---
+++ src/main/kotlin/PrintOnTerminal.kt
@@ -0,0 +1,12 @@
+fun main(){
+    println("Welcome the world of programming...😁")
+    print("Mohacel ")
+    print("Hosen")
+    println()
+    println("print() vs println()")
+    println("print()")
+    /*
+    *   print() method just print the statement
+    *   println() method print the statement then create new line(\n)
+    * */
+}
