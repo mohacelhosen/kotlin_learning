5cc7330f84a1490b8f9db3b710569a6c60a9d0dc
mohacelhosen <mohacel.hosen22@gmail.com>
2024-01-07 13:14:40 +0600

    Comment section

---
+++ src/main/kotlin/Comments.kt
@@ -0,0 +1,15 @@
+fun main(){
+//    println("Welcome the world of kotlin programming....😁")
+
+    /*
+    * multi
+    * line
+    * comment
+    * */
+
+    println("above line is commented 🤔")
+
+    println("\"//\" for single line comment")
+    println(" \"/* */\" for multi line comment")
+
+}
