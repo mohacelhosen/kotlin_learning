e8369448448dafa27e8c07a67097d94680ea4ea0
mohacelhosen <mohacel.hosen22@gmail.com>
2024-01-07 14:21:23 +0600

    Variable

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,18 @@
+
+fun main(){
+    var age:Int = 24
+    var phone:Long = 1946363345L
+    var height:Double = 5.56
+    var balance:Float = 3500F
+    var single:Boolean = true
+    var gender:Char = 'M'
+    var name:String = "Mohacel Hosen"
+
+    println("Name: $name")
+    println("Age: $age")
+    println("Gender: $gender")
+    println("Single: $single")
+    println("Height: $height")
+    println("phone: $phone")
+    println("Balance: $balance")
+}
