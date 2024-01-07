19e620b8fa1b6fe101521489e141a597c6b6a00a
mohacelhosen <mohacel.hosen22@gmail.com>
2024-01-07 14:29:45 +0600

    Var and Val

---
+++ src/main/kotlin/ValAndVar.kt
@@ -0,0 +1,10 @@
+
+fun main(){
+    val nameVal = "Mohacel"
+    var nameVar = "Mohacel"
+
+//    nameVal = "Mohacel Hosen" //Val cannot be reassigned
+    nameVar = "Mohacel Hosen"
+    println("Val:: $nameVal")
+    println("Var:: $nameVar")
+}
