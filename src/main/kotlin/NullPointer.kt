2c107fe08c17c42a3f9fa8091780e179b59108e9
mohacelhosen <mohacel.hosen22@gmail.com>
2024-01-07 15:02:59 +0600

    Null safty

--- src/main/kotlin/NullPointer.kt
+++ src/main/kotlin/NullPointer.kt
@@ -1,7 +1,18 @@
 
 fun main(){
-    var name:String? ="Mohacel Hosen"
-    println(name?.uppercase())
-    name=null;
-    println(name?.length)
+
+    var fullName:String?= "Mohacel Hosen"
+
+    println("Full name:: $fullName")
+    println("Total Character:: "+fullName?.length)
+    println("Upper Case:: "+fullName?.uppercase())
+    println("Lower Case:: "+fullName?.lowercase())
+
+    if (fullName != null) {
+        println("Access 6th character:: "+fullName.getOrNull(5))
+    }
+
+    println("Access 16th character:: "+fullName?.getOrNull(15))
+
+    println("Access 20th character:: "+fullName?.getOrNull(19))
 }
